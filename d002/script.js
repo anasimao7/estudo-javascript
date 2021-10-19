cf7eefd38f21f022a6d40cf92dcf469c7ef39256
Ana Simão <anasimaocontacto@gmail.com>
2021-10-19 17:22:50 +0100

    d002

---
+++ d002/script.js
@@ -0,0 +1,5 @@
+function perguntas(){
+    var nome = window.prompt("Como te chamas?");
+    var idade = window.prompt("E qual é a tua idade?");
+    window.alert("Prazer em conhecer-te " + nome + ", que tem " + idade + " anos!");
+}
