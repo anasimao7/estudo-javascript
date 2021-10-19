f735c2cf2ac0e63933dd472a2d866e8421f52ee4
Ana Simão <anasimaocontacto@gmail.com>
2021-10-19 16:48:21 +0100

    desafio 001

---
+++ d001/script.js
@@ -0,0 +1,11 @@
+function mensagem1(){
+    window.alert('Falei para não clicar! Não tem nada aqui...')
+}
+
+function mensagem2(){
+    window.alert('Obrigado por clicar em mim!')
+}
+
+function mensagem3(){
+    window.alert('Reparou que nenhuma mensagem é igual?')
+}
